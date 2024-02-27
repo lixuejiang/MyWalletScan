1925f53854296ebb1f1b23c85e06dc188bb4d3c7
lixuejiang <642898959@qq.com>
2024-02-27 21:35:03 +0800

    Update getVolume.js

--- src/utils/getZksyncData/getVolume.js
+++ src/utils/getZksyncData/getVolume.js
@@ -3,11 +3,11 @@ export const getVolume = (transactions) => {
     transactions.forEach((transaction) => {
         const transfers = transaction.transfers.sort(
             (a, b) =>
-                parseInt(b.amount) * 10 ** -b.token.decimals * b.token.price -
-                parseInt(a.amount) * 10 ** -a.token.decimals * a.token.price,
+                parseInt(b.amount) * 10 ** -b.token.decimals * b.token.usdPrice -
+                parseInt(a.amount) * 10 ** -a.token.decimals * a.token.usdPrice,
         );
         if (transfers.length === 0) return;
-        const tmpVolume = parseInt(transfers[0].amount) * 10 ** -transfers[0].token.decimals * transfers[0].token.price;
+        const tmpVolume = parseInt(transfers[0].amount) * 10 ** -transfers[0].token.decimals * transfers[0].token.usdPrice;
         volume += tmpVolume;
     });
     return volume === 0 ? 0 : volume.toFixed(2);
